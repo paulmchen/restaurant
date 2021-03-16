84d54f326741e0058d3bbeb534720d441fa19830
Paul Chen <paul.chen1@huawei.com>
2021-03-16 16:23:18 -0400

    update packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,7 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod back_of_house;
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-mod back_of_house {
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str)  -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
-    }
-
-    fn cook_order() {}
-}
+use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast
@@ -51,7 +12,8 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
-}
 
+    hosting::add_to_waitlist();
+}
 
 fn serve_order() {}
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use crate::Coin::{Nickle, Penny};
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method doing something
+    }
+}
+
+fn route(ip: IpAddrKind) {
+    println!("Ip kind.");
+}
+
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from .");
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+    route(home);
+    route(loopback);
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let v = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("The coin value is {}.", v);
+
+    let seven = Some(5);
+    let eight = plus_one(seven);
+    let none = plus_one(None);
+
+    let rec1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rec2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rec3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rec1 hold rect2? {}.", rec1.can_hold(&rec2));
+    println!("Can rec1 hold rect2? {}.", rec1.can_hold(&rec3));
+
+    let rec4 = Rectangle::square(3);
+    println!("Can rec1 hold rec4? {}.", rec1.can_hold(&rec4));
+}
