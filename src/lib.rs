17ff9941922f6f81ad1bbcefc6c3fee71fb895ab
Paul Chen <32553156+paulmchen@users.noreply.github.com>
2021-03-16 12:24:49 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,16 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {
+        pub fn add_to_waitlist() {}
 
-        }
-
-        fn seat_at_table() {
-
-        }
+        fn seat_at_table() {}
     }
 
     mod serving {
-        fn take_order() {
+        fn take_order() {}
 
-        }
+        fn serve_order() {}
 
-        fn serve_order() {
-
-        }
-
-        fn take_payment() {
-
-        }
+        fn take_payment() {}
     }
 }
 
@@ -49,9 +39,7 @@ mod back_of_house {
         super::serve_order();
     }
 
-    fn cook_order() {
-
-    }
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {
@@ -66,6 +54,4 @@ pub fn eat_at_restaurant() {
 }
 
 
-fn serve_order() {
-
-}
+fn serve_order() {}
